a5e9b60856e0bf952c333170ec7377daf23341cf
Jeff Hunken <jhunken@gmail.com>
2017-01-29 20:04:30 -0800

    add addToWatchlist to client

--- client/app/movie/movie.component.js
+++ client/app/movie/movie.component.js
@@ -5,15 +5,35 @@ import routing from './movie.routes';
 export class MovieController {
 
   /*@ngInject*/
-  constructor($http, $stateParams) {
+  constructor($http, $stateParams, watchlistService) {
     this.$http = $http;
     this.$stateParams = $stateParams;
+    this.watchlistService = watchlistService;
   }
 
   $onInit() {
     this.$http.get(`/api/movies/${this.$stateParams.id}`).then(response => {
       this.movie = response.data;
     });
+  }
+
+  addToWatchlist(id) {
+    // get watchlist
+    this.watchlistService.get()
+      .then(watchlistResponse => {
+        let watchlists = watchlistResponse.data;
+        // TODO: This needs to be more thoughtful than assuming the watchlist we want to add to is the first index the
+        // array.
+        let watchlist = watchlists[0];
+        watchlist.movies.push({_id: id});
+        this.$http.put(`/api/watchlists/${watchlist._id}`, watchlist)
+          .then(response => {
+            console.log(response);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      });
   }
 }
 
