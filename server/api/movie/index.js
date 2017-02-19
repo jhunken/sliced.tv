1e0879f6c6977fd4d334eb5cd82cbd1dde4c9853
Jeff Hunken <jhunken@gmail.com>
2017-02-19 15:41:23 -0800

    feat: do not require user to be authenticated for movies and search routes

--- server/api/movie/index.js
+++ server/api/movie/index.js
@@ -1,12 +1,11 @@
 'use strict';
-import * as auth from '../../auth/auth.service';
 let express = require('express');
 let controller = require('./movie.controller');
 
 let router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
-router.get('/all/:start/:limit/:sources/:platform', auth.isAuthenticated(), controller.index);
-router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/', controller.index);
+router.get('/all/:start/:limit/:sources/:platform', controller.index);
+router.get('/:id', controller.show);
 
 module.exports = router;
